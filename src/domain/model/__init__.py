24b0063826106f843219af2a1c3c4216cb877786
Anton Kudryavtsev <antonkudryavtsevdoem@gmail.com>
2024-06-14 23:32:32 +0300

    fix: update model package to export submodules

--- src/domain/model/__init__.py
+++ src/domain/model/__init__.py
@@ -3,4 +3,6 @@ Domain level models module.
 All business logic models and entities should be defined here.
 """
 
-__all__ = []
+from src.domain.model import allocation, form_field, format_entity, room, user
+
+__all__ = [allocation, form_field, format_entity, room, user]
