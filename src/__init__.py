eacd58b0764c3dd3f92077ee1d8217c831b75b95
Anton Kudryavtsev <antonkudryavtsevdoem@gmail.com>
2024-06-17 11:34:32 +0300

    fix: module imports

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,6 @@
 Random-API source code.
 """
 
-from src import adapters, app, domain, protocol, service, utils
+from src import adapter, app, domain, protocol, service, utils
 
-__all__ = [adapters, app, domain, protocol, service, utils]
+__all__ = [adapter, app, domain, protocol, service, utils]
