dadfd1577ade935d679ff35326061029f0c248e4
Anton Kudryavtsev <antonkudryavtsevdoem@gmail.com>
2024-07-03 16:50:11 +0300

    refactor: telegram auth adapter

--- src/adapter/external/auth/telegram.py
+++ src/adapter/external/auth/telegram.py
@@ -5,6 +5,7 @@ import hmac
 from datetime import datetime
 from typing import Any
 
+import jwt
 from pydantic import BaseModel, ValidationError
 
 from src.domain.exception import auth as auth_exception
@@ -12,27 +13,25 @@ from src.domain.exception import database as database_exception
 from src.domain.exception import service as service_exception
 from src.domain.model.scalar.object_id import ObjectID
 from src.domain.model.user import Profile, User
-from src.protocol.external.auth.oauth import OAuthContainer, OauthProtocol
+from src.protocol.external.auth.oauth import OAuthContainer, OAuthDTO, OauthProtocol
 from src.protocol.internal.database.user import (
     CreateUser,
     ReadUser,
 )
-from src.service.user.create import CreateUserService
+from src.service.user import UserService
 
 
-class TelegramUserProfileMixin(Profile): ...
+class TgUserProfileMixin(Profile): ...
 
 
-class TelegramAuthCallback(BaseModel):
+class TgOauthLoginCallback(BaseModel):
     id: int
     auth_date: datetime
     first_name: str | None
     last_name: str | None
-    username: str | None
+    username: str
     photo_url: str | None
     hash: str
-
-    profile: TelegramUserProfileMixin
 
     __field_order__ = [
         "auth_date",
@@ -51,45 +50,62 @@ class TelegramAuthCallback(BaseModel):
         return data_string
 
 
-class TelegramOauthContainer(BaseModel, OAuthContainer):
+class TgOauthRegisterCallback(TgOauthLoginCallback):
+    profile: TgUserProfileMixin
+
+
+class TgOauthDTO(OAuthDTO):
     id: ObjectID
     tid: int
 
-    @classmethod
-    def construct(cls, data: Any) -> TelegramOauthContainer:
-        return TelegramOauthContainer.model_validate(data, from_attributes=True)
 
-    def to_dto(self) -> TelegramOauthContainer:
+class TgOauthContainer(BaseModel, OAuthContainer):
+    jwt: str
+
+    @classmethod
+    def construct(cls, data: TgOauthDTO, secret: str) -> TgOauthContainer:
+        token = jwt.encode(data.model_dump(mode="json"), secret, algorithm="HS256")
+        self = cls(jwt=token)
         return self
+
+    def to_dto(self, secret: str) -> TgOauthDTO:
+        return TgOauthDTO.model_validate(
+            jwt.decode(self.jwt, secret, algorithms=["HS256"])
+        )
 
 
 class TelegramOauthAdapter(OauthProtocol):
     __secret_token: str
-    __service: CreateUserService
+    __jwt_secret: str
+    __service: UserService
 
-    def __init__(self, secret_token: str, service: CreateUserService):
+    def __init__(self, secret_token: str, jwt_secret: str, service: UserService):
         self.__secret_token = secret_token
+        self.__jwt_secret = jwt_secret
         self.__service = service
 
-    async def verify_callback_data(self, data: Any) -> OAuthContainer:
-        try:
-            data = TelegramAuthCallback.model_validate(data, from_attributes=True)
-        except ValidationError as e:
-            raise auth_exception.InvalidCredentialsException(
-                "failed to validate callback data"
-            ) from e
-
+    def __check_hash(
+        self, data: TgOauthRegisterCallback | TgOauthLoginCallback
+    ) -> None:
         signing = hmac.new(
-            self.__secret_token.encode(), data.to_data_string().encode(), hashlib.sha256
+            self.__secret_token.encode(),
+            data.to_data_string().encode(),
+            hashlib.sha256,
         )
         if signing.hexdigest() != data.hash:
             raise auth_exception.InvalidCredentialsException(
                 "data is malformed since the hash does not match"
             )
 
+    async def register(self, data: Any) -> TgOauthContainer:
         try:
+            request: TgOauthRegisterCallback = TgOauthRegisterCallback.model_validate(
+                data, from_attributes=True
+            )
+            self.__check_hash(request)
+
             user = await self.__service.create_user(
-                CreateUser(tid=data.id, profile=data.profile)
+                CreateUser(tid=request.id, profile=request.profile)
             )
         except (ValidationError, database_exception.ReflectUserException) as e:
             raise auth_exception.InvalidCredentialsException(
@@ -99,14 +115,45 @@ class TelegramOauthAdapter(OauthProtocol):
             raise auth_exception.UserAlreadyExistsException(
                 "creating new user failed"
             ) from e
+        except auth_exception.AuthException as e:
+            raise e
 
-        return TelegramOauthContainer(id=user.id, tid=user.tid)
+        return TgOauthContainer.construct(
+            TgOauthDTO(id=user.id, tid=user.tid), self.__jwt_secret
+        )
 
-    async def retrieve_user(self, data: OAuthContainer) -> User:
+    async def login(self, data: Any) -> TgOauthContainer:
         try:
-            data = TelegramOauthContainer.model_validate(data.to_dto())
+            request = TgOauthLoginCallback.model_validate(data, from_attributes=True)
+            self.__check_hash(request)
 
-            user = await self.__service.read_user(ReadUser(_id=data.id))
+            user = await self.__service.find_user_by_tid(request.id)
+        except (ValidationError, database_exception.ReflectUserException) as e:
+            raise auth_exception.InvalidCredentialsException(
+                "failed to reflect user data to read user"
+            ) from e
+        except service_exception.ReadUserException as e:
+            raise auth_exception.UserNotFoundException("reading user failed") from e
+        except auth_exception.AuthException as e:
+            raise e
+
+        return TgOauthContainer.construct(
+            TgOauthDTO(id=user.id, tid=user.tid), self.__jwt_secret
+        )
+
+    async def retrieve_user(self, data: TgOauthContainer) -> User:
+        try:
+            if not isinstance(data, TgOauthContainer):
+                raise auth_exception.InvalidCredentialsException("invalid data type")
+
+            try:
+                dto = data.to_dto(self.__jwt_secret)
+            except Exception as e:
+                raise auth_exception.InvalidCredentialsException(
+                    "invalid data type"
+                ) from e
+
+            user = await self.__service.read_user(ReadUser(_id=dto.id))
         except (ValidationError, AttributeError) as e:
             raise auth_exception.InvalidCredentialsException("invalid data type") from e
         except database_exception.ReflectUserException as e:
--- src/tests/test_adapters/test_oauth/test_telegram.py
+++ src/tests/test_adapters/test_oauth/test_telegram.py
@@ -10,18 +10,20 @@ from pydantic import BaseModel
 import src.domain.exception.auth as exception
 import src.protocol.external.auth as proto
 from src.adapter.external.auth.telegram import (
-    TelegramAuthCallback,
     TelegramOauthAdapter,
-    TelegramOauthContainer,
-    TelegramUserProfileMixin,
+    TgOauthContainer,
+    TgOauthDTO,
+    TgOauthRegisterCallback,
+    TgUserProfileMixin,
 )
 from src.adapter.internal.memorydb.service import MemoryDBAdapter
 from src.domain.model.scalar.object_id import ObjectID
 from src.domain.model.user import Gender, LanguageCode
 from src.protocol.internal.database.user import ReadUser
-from src.service.user.create import CreateUserService
+from src.service.user import UserService
 
 secret_token = secrets.token_hex(32)
+jwt_secret = secrets.token_hex(32)
 
 
 async def _get_telegram_oauth(
@@ -30,21 +32,21 @@ async def _get_telegram_oauth(
     if storage is None:
         storage = MemoryDBAdapter()
 
-    return TelegramOauthAdapter(secret_token, CreateUserService(storage))
+    return TelegramOauthAdapter(secret_token, jwt_secret, UserService(storage))
 
 
-async def test_verify_callback_data_ok():
+async def test_register_ok():
     storage = MemoryDBAdapter()
     actor = await _get_telegram_oauth(storage)
 
-    payload = TelegramAuthCallback(
+    payload = TgOauthRegisterCallback(
         id=123456789,
         auth_date=datetime.now().replace(microsecond=0),
         first_name="John",
         last_name="Doe",
         username="johndoe",
         photo_url="https://example.com/photo.jpg",
-        profile=TelegramUserProfileMixin(
+        profile=TgUserProfileMixin(
             first_name="John",
             last_name="Doe",
             username="johndoe",
@@ -60,13 +62,14 @@ async def test_verify_callback_data_ok():
     )
     payload.hash = signing.hexdigest()
 
-    container = await actor.verify_callback_data(payload)
+    container = await actor.register(payload)
 
-    assert isinstance(container, TelegramOauthContainer)
-    assert isinstance(container.id, ObjectID)
-    assert container.tid == payload.id
+    assert isinstance(container, TgOauthContainer)
+    dto = container.to_dto(jwt_secret)
+    assert isinstance(dto.id, ObjectID)
+    assert dto.tid == payload.id
 
-    user = await storage.read_user(ReadUser(_id=container.id))
+    user = await storage.read_user(ReadUser(_id=dto.id))
     assert user.tid == payload.id
     assert user.profile.first_name == payload.profile.first_name
     assert user.profile.last_name == payload.profile.last_name
@@ -76,18 +79,18 @@ async def test_verify_callback_data_ok():
     assert user.profile.gender == payload.profile.gender
 
 
-async def test_verify_callback_data__from_dict_ok():
+async def test_register_from_dict_ok():
     storage = MemoryDBAdapter()
     actor = await _get_telegram_oauth(storage)
 
-    payload = TelegramAuthCallback(
+    payload = TgOauthRegisterCallback(
         id=123456789,
         auth_date=datetime.now().replace(microsecond=0),
         first_name="John",
         last_name="Doe",
         username="johndoe",
         photo_url="https://example.com/photo.jpg",
-        profile=TelegramUserProfileMixin(
+        profile=TgUserProfileMixin(
             first_name="John",
             last_name="Doe",
             username="johndoe",
@@ -105,13 +108,14 @@ async def test_verify_callback_data__from_dict_ok():
 
     payload_data = json.loads(payload.model_dump_json())
 
-    container = await actor.verify_callback_data(payload_data)
+    container = await actor.register(payload_data)
 
-    assert isinstance(container, TelegramOauthContainer)
-    assert isinstance(container.id, ObjectID)
-    assert container.tid == payload.id
+    assert isinstance(container, TgOauthContainer)
+    dto = container.to_dto(jwt_secret)
+    assert isinstance(dto.id, ObjectID)
+    assert dto.tid == payload.id
 
-    user = await storage.read_user(ReadUser(_id=container.id))
+    user = await storage.read_user(ReadUser(_id=dto.id))
     assert user.tid == payload.id
     assert user.profile.first_name == payload.profile.first_name
     assert user.profile.last_name == payload.profile.last_name
@@ -121,7 +125,7 @@ async def test_verify_callback_data__from_dict_ok():
     assert user.profile.gender == payload.profile.gender
 
 
-async def test_verify_callback_data_invalid_input_data_error():
+async def test_register_invalid_input_data_error():
     actor = await _get_telegram_oauth()
 
     # missing a lot of fields
@@ -132,20 +136,20 @@ async def test_verify_callback_data_invalid_input_data_error():
     }
 
     with pytest.raises(exception.InvalidCredentialsException):
-        await actor.verify_callback_data(payload_data)
+        await actor.register(payload_data)
 
 
-async def test_verify_callback_data_hash_mismatch_error():
+async def test_register_hash_mismatch_error():
     actor = await _get_telegram_oauth()
 
-    payload = TelegramAuthCallback(
+    payload = TgOauthRegisterCallback(
         id=123456789,
         auth_date=datetime.now().replace(microsecond=0),
         first_name="John",
         last_name="Doe",
         username="johndoe",
         photo_url="https://example.com/photo.jpg",
-        profile=TelegramUserProfileMixin(
+        profile=TgUserProfileMixin(
             first_name="John",
             last_name="Doe",
             username="johndoe",
@@ -164,20 +168,20 @@ async def test_verify_callback_data_hash_mismatch_error():
     payload.last_name = "XXX"  # data is malformed
 
     with pytest.raises(exception.InvalidCredentialsException):
-        await actor.verify_callback_data(payload)
+        await actor.register(payload)
 
 
-async def test_verify_callback_data_reflect_error():
+async def test_register_reflect_error():
     actor = await _get_telegram_oauth()
 
-    payload = TelegramAuthCallback(
+    payload = TgOauthRegisterCallback(
         id=123456789,
         auth_date=datetime.now().replace(microsecond=0),
         first_name="John",
         last_name="Doe",
         username="johndoe",
         photo_url="https://example.com/photo.jpg",
-        profile=TelegramUserProfileMixin(
+        profile=TgUserProfileMixin(
             first_name="John",
             last_name="Doe",
             username="johndoe",
@@ -203,21 +207,21 @@ async def test_verify_callback_data_reflect_error():
     )  # type: ignore
 
     with pytest.raises(exception.InvalidCredentialsException):
-        await actor.verify_callback_data(payload)
+        await actor.register(payload)
 
 
-async def test_verify_callback_data_user_exists_error():
+async def test_register_user_exists_error():
     storage = MemoryDBAdapter()
     actor = await _get_telegram_oauth(storage)
 
-    payload = TelegramAuthCallback(
+    payload = TgOauthRegisterCallback(
         id=123456789,
         auth_date=datetime.now().replace(microsecond=0),
         first_name="John",
         last_name="Doe",
         username="johndoe",
         photo_url="https://example.com/photo.jpg",
-        profile=TelegramUserProfileMixin(
+        profile=TgUserProfileMixin(
             first_name="John",
             last_name="Doe",
             username="johndoe",
@@ -235,13 +239,14 @@ async def test_verify_callback_data_user_exists_error():
 
     payload_data = json.loads(payload.model_dump_json())
 
-    container = await actor.verify_callback_data(payload_data)
+    container = await actor.register(payload_data)
 
-    assert isinstance(container, TelegramOauthContainer)
-    assert isinstance(container.id, ObjectID)
-    assert container.tid == payload.id
+    assert isinstance(container, TgOauthContainer)
+    dto = container.to_dto(jwt_secret)
+    assert isinstance(dto.id, ObjectID)
+    assert dto.tid == payload.id
 
-    user = await storage.read_user(ReadUser(_id=container.id))
+    user = await storage.read_user(ReadUser(_id=dto.id))
     assert user.tid == payload.id
     assert user.profile.first_name == payload.profile.first_name
     assert user.profile.last_name == payload.profile.last_name
@@ -252,20 +257,20 @@ async def test_verify_callback_data_user_exists_error():
 
     # creating the same useg again result in an error
     with pytest.raises(exception.UserAlreadyExistsException):
-        await actor.verify_callback_data(payload)
+        await actor.register(payload)
 
 
 async def test_retrieve_user_ok():
     actor = await _get_telegram_oauth()
 
-    payload = TelegramAuthCallback(
+    payload = TgOauthRegisterCallback(
         id=123456789,
         auth_date=datetime.now().replace(microsecond=0),
         first_name="John",
         last_name="Doe",
         username="johndoe",
         photo_url="https://example.com/photo.jpg",
-        profile=TelegramUserProfileMixin(
+        profile=TgUserProfileMixin(
             first_name="John",
             last_name="Doe",
             username="johndoe",
@@ -281,7 +286,7 @@ async def test_retrieve_user_ok():
     )
     payload.hash = signing.hexdigest()
 
-    container = await actor.verify_callback_data(payload)
+    container = await actor.register(payload)
 
     user = await actor.retrieve_user(container)
 
@@ -315,4 +320,153 @@ async def test_retrieve_user_not_found_error():
     actor = await _get_telegram_oauth()
 
     with pytest.raises(exception.UserNotFoundException):
-        await actor.retrieve_user(TelegramOauthContainer(id=ObjectID(), tid=0))
+        await actor.retrieve_user(
+            TgOauthContainer.construct(TgOauthDTO(id=ObjectID(), tid=0), jwt_secret)
+        )
+
+
+async def test_retrieve_user_invalid_jwt_error():
+    actor = await _get_telegram_oauth()
+
+    with pytest.raises(exception.InvalidCredentialsException):
+        await actor.retrieve_user(TgOauthContainer(jwt="xxx"))
+
+
+async def test_login_ok():
+    storage = MemoryDBAdapter()
+    actor = await _get_telegram_oauth(storage)
+
+    payload = TgOauthRegisterCallback(
+        id=123456789,
+        auth_date=datetime.now().replace(microsecond=0),
+        first_name="John",
+        last_name="Doe",
+        username="johndoe",
+        photo_url="https://example.com/photo.jpg",
+        profile=TgUserProfileMixin(
+            first_name="John",
+            last_name="Doe",
+            username="johndoe",
+            birthdate=datetime.today().date(),
+            language_code=LanguageCode.RU,
+            gender=Gender.MALE,
+        ),
+        hash="",
+    )
+
+    signing = hmac.new(
+        secret_token.encode(), payload.to_data_string().encode(), hashlib.sha256
+    )
+    payload.hash = signing.hexdigest()
+
+    await actor.register(payload)
+
+    container: TgOauthContainer = await actor.login(payload)  # type: ignore
+
+    dto = container.to_dto(jwt_secret)
+    assert isinstance(dto.id, ObjectID)
+    assert dto.tid == payload.id
+
+    user = await storage.read_user(ReadUser(_id=dto.id))
+    assert user.tid == payload.id
+    assert user.profile.first_name == payload.profile.first_name
+    assert user.profile.last_name == payload.profile.last_name
+    assert user.profile.username == payload.profile.username
+    assert user.profile.birthdate == payload.profile.birthdate
+    assert user.profile.language_code == payload.profile.language_code
+    assert user.profile.gender == payload.profile.gender
+
+
+async def test_login_from_dict_ok():
+    storage = MemoryDBAdapter()
+    actor = await _get_telegram_oauth(storage)
+
+    payload = TgOauthRegisterCallback(
+        id=123456789,
+        auth_date=datetime.now().replace(microsecond=0),
+        first_name="John",
+        last_name="Doe",
+        username="johndoe",
+        photo_url="https://example.com/photo.jpg",
+        profile=TgUserProfileMixin(
+            first_name="John",
+            last_name="Doe",
+            username="johndoe",
+            birthdate=datetime.today().date(),
+            language_code=LanguageCode.RU,
+            gender=Gender.MALE,
+        ),
+        hash="",
+    )
+
+    signing = hmac.new(
+        secret_token.encode(), payload.to_data_string().encode(), hashlib.sha256
+    )
+    payload.hash = signing.hexdigest()
+
+    payload_data = json.loads(payload.model_dump_json())
+    await actor.register(payload_data)
+
+    container: TgOauthContainer = await actor.login(payload_data)  # type: ignore
+    dto = container.to_dto(jwt_secret)
+    assert isinstance(dto.id, ObjectID)
+    assert dto.tid == payload.id
+
+    user = await storage.read_user(ReadUser(_id=dto.id))
+    assert user.tid == payload.id
+    assert user.profile.first_name == payload.profile.first_name
+    assert user.profile.last_name == payload.profile.last_name
+    assert user.profile.username == payload.profile.username
+    assert user.profile.birthdate == payload.profile.birthdate
+    assert user.profile.language_code == payload.profile.language_code
+    assert user.profile.gender == payload.profile.gender
+
+
+async def test_login_user_not_found_error():
+    storage = MemoryDBAdapter()
+    actor = await _get_telegram_oauth(storage)
+
+    payload = TgOauthRegisterCallback(
+        id=123456789,
+        auth_date=datetime.now().replace(microsecond=0),
+        first_name="John",
+        last_name="Doe",
+        username="johndoe",
+        photo_url="https://example.com/photo.jpg",
+        profile=TgUserProfileMixin(
+            first_name="John",
+            last_name="Doe",
+            username="johndoe",
+            birthdate=datetime.today().date(),
+            language_code=LanguageCode.RU,
+            gender=Gender.MALE,
+        ),
+        hash="",
+    )
+
+    signing = hmac.new(
+        secret_token.encode(), payload.to_data_string().encode(), hashlib.sha256
+    )
+    payload.hash = signing.hexdigest()
+
+    payload_data = json.loads(payload.model_dump_json())
+
+    with pytest.raises(exception.UserNotFoundException):
+        await actor.login(payload_data)
+
+
+async def test_login_invalid_input_data_error():
+    actor = await _get_telegram_oauth()
+
+    with pytest.raises(exception.InvalidCredentialsException):
+        await actor.login(object)  # type: ignore
+
+
+async def test_login_reflect_error():
+    actor = await _get_telegram_oauth()
+
+    class InvalidData:
+        id: object = object
+
+    with pytest.raises(exception.InvalidCredentialsException):
+        await actor.login(InvalidData())  # type: ignore
