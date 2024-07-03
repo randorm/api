3f04e695b507d18bb9ccdf6430d829026480e611
Anton Kudryavtsev <antonkudryavtsevdoem@gmail.com>
2024-07-03 16:48:55 +0300

    refactor: user service

--- src/service/user.py
+++ src/service/user.py
@@ -1,9 +1,9 @@
 import src.domain.model as domain
 import src.protocol.internal.database.user as proto
-from src.domain.exception.service import CreateUserException
+from src.domain.exception.service import CreateUserException, ReadUserException
 
 
-class CreateUserService:
+class UserService:
     def __init__(self, user_repository: proto.UserDatabaseProtocol):
         self.__user_repository = user_repository
 
@@ -20,6 +20,21 @@ class CreateUserService:
             raise CreateUserException(
                 f"failed to reflect user type with error: {e}"
             ) from e
+
+    async def exists(self, user_tid: int) -> bool:
+        users = await self.__user_repository.find_users(
+            proto.FindUsersByTid(tid=user_tid)
+        )
+        return len(users) > 0
+
+    async def find_user_by_tid(self, user_tid: int) -> domain.User:
+        users = await self.__user_repository.find_users(
+            proto.FindUsersByTid(tid=user_tid)
+        )
+        if len(users) > 0:
+            return users[0]
+        else:
+            raise ReadUserException("user not found")
 
     async def read_user(self, user: proto.ReadUser) -> domain.User:
         return await self.__user_repository.read_user(user)
